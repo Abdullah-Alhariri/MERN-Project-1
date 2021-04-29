e80424a331414e9cef1f41292144fbbf91f5deaf
Abdullah Alhariri <alhariri.abdu.2018@gmail.com>
2021-04-29 03:11:47 +0200

    JWTBackend

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 
 import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
+app.use("/user", userRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://abdullah:abdullah@cluster0.g624b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
@@ -19,11 +21,7 @@ const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() =>
-    app.listen(PORT, () =>
-      console.log(`Server Running on Port: http://localhost:${PORT}`)
-    )
-  )
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set("useFindAndModify", false);
